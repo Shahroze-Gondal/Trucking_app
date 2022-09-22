0bb7f79ccf0bc71d3ac6cff555cd6ceb10924f51
Shahroze999 <111286923+Shahroze999@users.noreply.github.com>
2022-09-22 19:14:07 +0500

    Delete tutorial directory

--- tutorial/trucking_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Dispatch, Order
-# Register your models here.
-
-admin.site.register(Dispatch)
-admin.site.register(Order)
--- tutorial/trucking_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TruckingAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trucking_app'
